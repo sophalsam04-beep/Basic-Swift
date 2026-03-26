180f8a6d42a452724a989b21ad58232701829e56
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-03-26 20:39:53 +0700

    Add files via upload

---
+++ Operator.swift
@@ -0,0 +1,15 @@
+// Learning Opertor in swift programming
+
+var likeCount : Double = 5
+var commentCount : Double = 0
+var viewCount : Double = 100
+
+// using to operator
+// addition
+likeCount = likeCount + 1
+likeCount+=1
+
+// substraction
+likeCount = likeCount - 1
+
+
