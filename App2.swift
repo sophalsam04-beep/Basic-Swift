878b876aa50e18e0689a99ff95a9f194fe258d77
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-03-26 20:41:35 +0700

    Add files via upload

---
+++ App2.swift
@@ -0,0 +1,44 @@
+var likeCount : Double = 5
+var commentCount : Double = 0
+var viewCount : Double = 100
+
+// Additon
+likeCount = likeCount + 1
+likeCount+=1
+
+// substraction
+likeCount = likeCount - 1
+likeCount-=1
+// Multiplication
+likeCount = lik=eCount * 2
+likeCount*=2
+
+// division
+likeCount = likeCount / 2
+likeCount/=2
+
+// if else statement
+var iscount : Bool = true
+if iscount == true{
+    print("Student is count succesfully !")
+}else{
+    print("Student can't count !");
+    
+}
+
+if likeCount >3{
+    print("People like 3 post")
+}else{
+    print("People cannot like 3 post")
+    
+}
+
+if(likeCount == 3){
+    print("people have 3 post")
+    
+}
+
+if likeCount>=3 && likeCount!=0{
+    
+    
+}
---
+++ tempCodeRunnerFile.swift
@@ -0,0 +1,7 @@
+// if else statement
+// var iscount : Bool = true
+// if iscount == true{
+//     print("Student is count succesfully !")
+// }else{
+//     print("Student can't count !");
+    
