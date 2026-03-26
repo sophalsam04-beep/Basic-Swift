0676f507ebf90ba7d631ab2d48152e26ce520b3f
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-03-26 20:33:34 +0700

    Add repeat and while loop examples in Swift
    Implement repeat and while loops to demonstrate looping in Swift.

---
+++ 14_Repeat_loop.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+
+var a = 1;
+repeat{
+    print(a)
+    a+=1
+}while a<=10;
+
+var x = 1.2;
+repeat{
+    print("Hello swift !")
+}while x<=100;
+
+var i = 1
+var f = 1
+while i<=3{
+    
+    
+    while f<=3{
+        print("i=\(i) and f =\(f)")
+        f+=1
+    }
+    i+=1
+}
