1f94e3ff2a4ee441d59a3973387aded66ff8a446
Phal Developer <sophalsam04@gmail.com>
2026-03-31 20:35:21 +0700

    Implement dynamic button creation in ContentView
    Added dynamic buttons for names, universities, colors, and fruits.

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,11 @@
+// Date : 31-03-2026
+// Name : SAM SOPHAL
+// Role : Mobile Apps developments
+
 import SwiftUI
 
 struct ContentView : View {
+    // Adding state for a variable
     @State private var username : String = ""
     @State private var password : String = ""
     @State private var showalert = false
@@ -10,6 +15,12 @@ struct ContentView : View {
     @State private var mul_buttons = false
     @State private var ch_buttons = false
     @State private var texts = ""
+    let name_list = ["Un virak","vatanak","Tola","Vuntoeun","Cheatra","Sokun","Nita","Cheat"];
+    let universitys = ["NU","USH","BBU","RUPP","NUM","CUS"];
+    let colors = ["GREEN","BLUE","RED","WHITE","BLACK","WHITE"];
+    let fruits = ["Banana","Cherry","Orange","Duran","Lettuce"];
+    
+    // Dynamic Buttons
     var body: some View {
         
         // Showing alert message
@@ -79,6 +90,45 @@ struct ContentView : View {
                 Button("Change Now"){}
             }message: {
                 Text("Change Succesfully!")
+            }
+        }
+        
+        
+        
+        // Using stacking Up Buttons --> showing Multiple name
+        VStack(spacing : 25){
+            // stacking up buttons
+            ForEach(name_list, id: \.self){ item in
+                Button(item){
+                    print("Succesfully !")
+                }
+                
+            }
+        }
+        
+        
+        // Espresio 2
+        
+        HStack{
+            // stacking up buttons
+            ForEach(universitys, id: \.self){ u in
+                Button(u){
+                    print("Succesfully !");
+                }
+            }
+        }
+        HStack{
+            ForEach(colors, id: \.self){ i in
+                Button(i){
+                    print("Showing Result")
+                }
+            }
+        }
+        HStack{
+            ForEach(fruits, id: \.self){ a in
+                Button(a){
+                    print("Displaying result!");
+                }
             }
         }
         .ignoresSafeArea()
