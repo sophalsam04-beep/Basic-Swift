c1dc9ded2dde18fb9feb753518dc5c6030f45f75
Phal Developer <sophalsam04@gmail.com>
2026-04-07 22:26:51 +0700

    Add custom layout with circles and welcome text
    Implement a custom layout using GeometryReader and VStack.

---
+++ 36_Custom_Layouts.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        // Custom Layout
+        GeometryReader{
+            geo in
+            let width = geo.size.width / 3
+            VStack(spacing : 25){
+                ForEach(0..<5){
+                    row in
+                    HStack(spacing : 25){
+                        ForEach(0..<5){
+                            col in
+                            Circle()
+                                .fill(.blue)
+                                .frame(width: width.binade,height: width.binade)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            Text("Welcome to swift")
+                .alignmentGuide(.leading) {d in d[.leading]}
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
